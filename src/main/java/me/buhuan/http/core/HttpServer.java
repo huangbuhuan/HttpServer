bb7f55e4d30112765d353d7f74ea71b9f153ef89
黄步欢 <buhuanhuang@gmail.com>
2018-01-28 23:16:32 +0800

    添加HttpServer类

--- src/main/java/me/buhuan/http/core/HttpServer.java
+++ src/main/java/me/buhuan/http/core/HttpServer.java
@@ -1,8 +1,39 @@
 package me.buhuan.http.core;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+
+
 public class HttpServer {
 
-	public void start(int port) {
-
+	public void start(int port) throws InterruptedException {
+		EventLoopGroup parentGroup = new NioEventLoopGroup();
+		EventLoopGroup childGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(parentGroup, childGroup)
+				.channel(NioServerSocketChannel.class)
+				.option(ChannelOption.SO_BACKLOG, 128)
+				.childOption(ChannelOption.SO_KEEPALIVE, true)
+				.childHandler(new ChannelInitializer<SocketChannel>() {
+					@Override
+					protected void initChannel(SocketChannel ch) {
+						ChannelPipeline pipeline = ch.pipeline();
+						pipeline.addLast(new HttpResponseEncoder())
+							.addLast(new HttpRequestDecoder())
+							.addLast(new HttpServerHandler());
+					}
+				});
+			ChannelFuture future = bootstrap.bind(port).sync();
+			future.channel().closeFuture().sync();
+		} finally {
+			parentGroup.shutdownGracefully();
+			childGroup.shutdownGracefully();
+		}
 	}
 }
---
+++ src/main/java/me/buhuan/http/core/HttpServerHandler.java
@@ -0,0 +1,7 @@
+package me.buhuan.http.core;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class HttpServerHandler extends ChannelInboundHandlerAdapter {
+
+}
